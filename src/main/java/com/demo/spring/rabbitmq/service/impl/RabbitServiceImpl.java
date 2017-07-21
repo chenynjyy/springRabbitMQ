cd09f45ef78355531f91fa6fa4b66daa471b3f69
chenyunan <chenyunan@banggood.cn>
2017-07-21 18:53:12 +0800

    修改

--- src/main/java/com/demo/spring/rabbitmq/service/impl/RabbitServiceImpl.java
+++ src/main/java/com/demo/spring/rabbitmq/service/impl/RabbitServiceImpl.java
@@ -3,6 +3,10 @@ package com.demo.spring.rabbitmq.service.impl;
 import com.demo.spring.rabbitmq.service.RabbitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,9 +24,14 @@ public class RabbitServiceImpl implements RabbitService {
 
     public void send(String exchange, String routingKey, Object message) {
 
-        Object recvMessage = rabbitTemplate.convertSendAndReceive(exchange, routingKey, message);
-        logger.info("something : " + recvMessage);
-//        rabbitTemplate.
+        Object recvMessage = rabbitTemplate.convertSendAndReceive(exchange, routingKey, message, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                MessageProperties messageProperties = message.getMessageProperties();
+                return message;
+            }
+        });
+        logger.info("something : " + new String((byte[]) recvMessage));
 
     }
 
